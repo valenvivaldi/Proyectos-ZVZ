a3af70f5a84de038696a75d0dbc83ea1a86f054b
valenvivaldi <valenvivaldi@gmail.com>
2017-10-11 14:38:09 -0300

    repo revivido

---
+++ ProyectoDBII/src/Main.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		Properties prop = new Properties();
+		InputStream is = null;
+		try {
+			is = new FileInputStream("./src/config.PROPERTIES");
+			prop.load(is);
+		} catch(IOException e) {
+			System.out.println(e.toString());
+		}
+
+		String driver = prop.getProperty("driver");
+		System.out.println(driver);
+		String protocolo = prop.getProperty("protocolo");
+		System.out.println(protocolo);
+		String usuario = prop.getProperty("usuario");
+		System.out.println(usuario);
+		String password = prop.getProperty("pass");
+		System.out.println(password);
+		String host = prop.getProperty("host");
+		System.out.println(host);
+		String nombre_base1 = prop.getProperty("nombre_base1");
+		System.out.println(nombre_base1);
+		String nombre_base2 = prop.getProperty("nombre_base2");
+		System.out.println(nombre_base2);
+		
+		String url = "jdbc:"+protocolo+"://"+host+"/"+nombre_base1;
+		String url2 = "jdbc:"+protocolo+"://"+host+"/"+nombre_base2;
+		System.out.println("string de coneccion para la 1ra base ");
+		System.out.println(url);
+		System.out.println(url2);
+		System.out.println("string de coneccion para la 2da base ");
+
+	}
+}
