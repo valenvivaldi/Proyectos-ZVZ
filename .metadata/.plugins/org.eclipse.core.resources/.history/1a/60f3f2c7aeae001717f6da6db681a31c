a2b2166dc838e73e7fe4326c1a5d7f49f62503b2
valenvivaldi <valenvivaldi@gmail.com>
2017-10-12 16:51:25 -0300

    correccion clave foranea

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/1a/60f3f2c7aeae001717f6da6db681a31c
@@ -0,0 +1,30 @@
+
+public class Atributo {
+private String tipo;
+private String nombre;
+
+
+
+
+
+
+
+
+
+public String getTipo() {
+	return tipo;
+}
+
+public void setTipo(String tipo) {
+	this.tipo = tipo;
+}
+
+public String getNombre() {
+	return nombre;
+}
+
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+
+}
--- ProyectoDBII/src/Main.java
+++ ProyectoDBII/src/Main.java
@@ -26,13 +26,16 @@ public class Main {
 		System.out.println(nombre_base1);
 		String nombre_base2 = prop.getProperty("nombre_base2");
 		System.out.println(nombre_base2);
-		
+
 		String url = "jdbc:"+protocolo+"://"+host+"/"+nombre_base1;
 		String url2 = "jdbc:"+protocolo+"://"+host+"/"+nombre_base2;
 		System.out.println("string de coneccion para la 1ra base ");
 		System.out.println(url);
 		System.out.println(url2);
 		System.out.println("string de coneccion para la 2da base ");
+
+		Base base1 = new Base(nombre_base1);
+		Base base2 = new Base(nombre_base2);
 
 	}
 }
