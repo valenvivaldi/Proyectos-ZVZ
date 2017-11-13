982829f3ee3ee3c06ded4d4a3f22c2facd25771a
bzergoni <bruno_z_c@hotmail.com>
2017-11-13 00:08:42 -0300

    Agregado de procedimientos

--- ProyectoDBII/pruebaBase4.sql
+++ ProyectoDBII/pruebaBase4.sql
@@ -14,6 +14,21 @@ create table Perro(idanimal integer, nombre varchar(50),color varchar(50), raza 
     r := param + p;
   END;
   $$ LANGUAGE plpgsql;
+  
+  CREATE OR REPLACE FUNCTION p1(IN param int, IN p int, in r int, IN re date) returns void AS --parametro con mismo nombre pero distinto tipo
+  $$
+  BEGIN
+    r := param + p;
+  END;
+  $$ LANGUAGE plpgsql;
+  
+  
+  CREATE OR REPLACE FUNCTION p2(IN param int, IN p int, in r int, IN re imt) returns int AS --mismo nombre de procedure. Distinto tipo de retorno
+  $$
+  BEGIN
+    r := param + p;
+  END;
+  $$ LANGUAGE plpgsql;
 ----------------------------------------------------------------------------------------------
 
 create schema basep2;
@@ -28,6 +43,20 @@ create table Gato(idanimal integer, nombre varchar(50),color varchar(50), raza v
 create table Perro(idanimal integer, nombre varchar(50),color varchar(50), raza varchar(50), constraint pkperro primary key(idanimal),constraint fkperro foreign key(idanimal) references Animal(idanimal));
 
   CREATE OR REPLACE FUNCTION p(IN param int, IN p int, IN r int) returns void AS
+  $$
+  BEGIN
+    r := param + p;
+  END;
+  $$ LANGUAGE plpgsql;
+  
+    CREATE OR REPLACE FUNCTION p1(IN param int, IN p int, in r int, IN re int) returns void AS
+  $$
+  BEGIN
+    r := param + p;
+  END;
+  $$ LANGUAGE plpgsql;
+  
+  CREATE OR REPLACE FUNCTION p2(IN param int, IN p int, in r int, IN re imt) returns void AS
   $$
   BEGIN
     r := param + p;
